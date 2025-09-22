cbc8f198b2b57cdca17ecef9c3826773b7f8a757
Qurclinc <pastghost2@mail.ru>
2025-09-22 18:43:43 +0500

    Diopantine equation added; logic a bit better

--- app/handlers/crypto_handlers.py
+++ app/handlers/crypto_handlers.py
@@ -4,7 +4,8 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from html import escape
 
-from app.logic import find_values, do_PAE, do_OAE
+from app.backend.Euclid import find_values, do_PAE, do_OAE
+from app.backend.Diophantine import solve_diophantine_equation
 import app.keyboards as kb
 
 router = Router()
@@ -15,10 +16,16 @@ class OAE(StatesGroup):
 class PAE(StatesGroup):
     numbers = State()
 
+class Diophantine(StatesGroup):
+    coefficients = State()
+
 @router.message(Command("start"))
 async def start(message: types.Message):
     await message.answer("aboba", reply_markup=kb.main_kb)
 
+###############
+### OAE alg ###
+###############
 @router.message(F.text == "ОАЕ")
 async def message_OAE(message: types.Message, state: FSMContext):
     await message.answer("Введите числа разделённые пробелами")
@@ -37,8 +44,11 @@ async def make_OAE(message: types.Message, state: FSMContext):
     safe = escape(text)
 
     await state.clear()
-    await message.answer(f"<pre>{safe}</pre>", parse_mode="HTML")
+    await message.answer(f"<pre>{safe}</pre>", parse_mode="HTML", reply_markup=kb.main_kb)
 
+###############
+### PAE alg ###
+###############
 
 @router.message(F.text == "РАЕ")
 async def message_PAE(message: types.Message, state: FSMContext):
@@ -57,7 +67,47 @@ async def make_PAE(message: types.Message, state: FSMContext):
     values = find_values(a, b)
     text = do_PAE(values)
 
-    # Экранируем для HTML и отправляем в <pre> чтобы сохранить моноширинность и выравнивание
     safe = escape(text)
     await state.clear()
-    await message.answer(f"<pre>{safe}</pre>", parse_mode="HTML")
+    await message.answer(f"<pre>{safe}</pre>", parse_mode="HTML", reply_markup=kb.main_kb)
+
+#######################
+### Diophantine alg ###
+#######################
+
+@router.message(F.text == "Диофантовое уравнение")
+async def message_Diophantine(message: types.Message, state: FSMContext):
+    await message.answer("Введите числа разделённые пробелами соответствующие коэффициентам\n``` ax + by = c```\nСоответствующие коэффициентам a, b и c соответственно.\n\nНапример: `439 118 3` для уравнения 439x + 118y = 3", parse_mode="Markdown")
+    await state.set_state(Diophantine.coefficients)
+    
+@router.message(Diophantine.coefficients)
+async def make_Diophantine(message: types.Message, state: FSMContext):
+    # ожидаем три числа: a, b и c
+    try:
+        a, b, c = map(int, message.text.split())
+    except ValueError:
+        await message.answer("Неверный ввод. Введите три целых числа через пробел, например: `439 118 3`", parse_mode="Markdown")
+        return
+    result = solve_diophantine_equation(a, b, c)
+    text = f"{a}x + {b}y = {c}\n\n"
+    try:
+        if result["result"] == "Нет решений":
+            message.answer(result["answer"])
+            return
+    except KeyError:
+        pass
+    text += f"({a}, {b}) = {result["gcd"]}\n"
+    text += f"{c} ⫶ {result["gcd"]} => ∃ решение\n\n"
+    text += do_PAE(result["table"])
+    text += f"\n\nНОД({a}, {b}) = {result["gcd"]} = {a} * {result["k1"]} + {b} * {result["k2"]}\n"
+    x0, y0 = result["part_solution"]
+    if result["multiplicated"]:
+        text += f"{a} * {result["k1"]} + {b} * {result["k2"]} = 1 | * {c}\n"
+    text += f"{a} * {x0} + {b} * {y0} = {c}\n\n"
+    text += f"Частное решение:\nX̄ = {x0}\nɏ = {y0}\n\n"
+    sol = result["common_solution"]
+    text += f"Общее решение:\nx = {sol["x"][0]} + {sol["x"][1]}t\ny = {sol["y"][0]} - {sol["y"][1]}t\nt ∈ Z\n"
+
+    safe = escape(text)
+    await state.clear()
+    await message.answer(f"<pre>{safe}</pre>", parse_mode="HTML", reply_markup=kb.main_kb)
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,5 +1,6 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
 main_kb = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text="ОАЕ"), KeyboardButton(text="РАЕ")]
+    [KeyboardButton(text="ОАЕ"), KeyboardButton(text="РАЕ")],
+    [KeyboardButton(text="Диофантовое уравнение")]
 ], resize_keyboard=True)
