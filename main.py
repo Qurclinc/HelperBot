eb10cfb8a8632c75c616884fec7b051706cd27db
Qurclinc <pastghost2@mail.ru>
2025-09-25 12:26:09 +0500

    router wasn't included XD

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import asyncio
 from config import bot, dp
 from app.handlers.crypto_handlers import router as crypto_router
+from app.handlers.general_handlers import router as general_router
 
 async def main():
     dp.include_router(crypto_router)
+    dp.include_router(general_router)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
